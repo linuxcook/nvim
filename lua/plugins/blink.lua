6a82510d6ebf48473bbf486aedeaf24e0265a770
linuxcook <linusxcook@gmail.com>
2025-04-09 23:31:36 -0400

    Modify blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@
 
 return {
   'saghen/blink.cmp',
-  version = '*',
+  version = '1.*',
   build = 'cargo build --release',
 
   ---@module 'blink.cmp'
@@ -17,8 +17,14 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
     },
+    completion = {
+      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+
+      ghost_text = { enabled = true },
+    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+    fuzzy = { implementation = 'prefer_rust' },
   },
 }
